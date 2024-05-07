6b08ac6a9af562769d9d8e16fe4d88271322528c
Kaan Akın <kaanhakan2005@gmail.com>
2024-05-07 17:49:50 +0300

    refactor(task-4): added create form request

--- task-4/PostController.php
+++ task-4/PostController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreatePostRequest;
 use App\Services\PostService;
-use App\Exceptions\PostCreationException;
-use App\Exceptions\InvalidParameterException;
 
 class PostController extends Controller
 {
@@ -16,18 +14,10 @@ class PostController extends Controller
         $this->postService = $postService;
     }
 
-    public function createPost(Request $request)
+    public function createPost(CreatePostRequest $request)
     {
         try {
-            $authorId = $request->input('author_id');
-            $title = $request->input('title');
-            $categoryId = $request->input('category_id');
-
-            if (!$authorId || !$title || !$categoryId) {
-                throw new InvalidParameterException("Missing or invalid parameters.");
-            }
-
-            $message = $this->postService->createPost((int) $authorId, $title, (int) $categoryId);
+            $message = $this->postService->createPost($createPostRequest->validated());
             return response()->json(['message' => $message], 201);
         } catch (InvalidParameterException $e) {
             return response()->json(['error' => $e->getMessage()], $e->getCode());
